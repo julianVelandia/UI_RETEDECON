68eaf0f79655d8c952d708d6dcb52d30b8869f7b
Julián Camilo Velandia <jvelandiag@unal.edu.co>
2021-03-16 18:15:34 -0500

    Addelanto interfas gráfica

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+import time
 
 
 class MainWindow(QMainWindow): #Ventana principal
@@ -15,65 +16,99 @@ class MainWindow(QMainWindow): #Ventana principal
         with open("style.css") as f:
             self.setStyleSheet(f.read())
 
-
+        '''
+        Configuraciones básicas
+        '''
         self.setMinimumSize(1024,600)    #tamaño mínimo
         self.setMaximumSize(1024,600)  #tamaño máximo
         self.setWindowTitle('RETEDECON')   #titulo
-        self.setWindowIcon(QIcon("static/favicon.png"))
+        self.setWindowIcon(QIcon("static/favicon.png"))   #Favicon
         
         self.centralWidget = QWidget()
         self.setCentralWidget(self.centralWidget)
-        self.centralWidget.setObjectName("window")    #nombre que enlaza en css 
+        self.centralWidget.setObjectName("window") #nombre que enlaza en css 
         
-        self.label = QLabel(self)
-        self.label.setGeometry(288.9,-10,1024,600)
-        self.pixmap = QPixmap('static/Logo_central.png')
-        self.label.setPixmap(self.pixmap)
+        self.label_img_central = QLabel(self)
+        self.label_img_central.setGeometry(288.9,-10,1024,600)
+        self.pixmap = QPixmap('static/Logo_central.png')   #Imagen central
+        self.label_img_central.setPixmap(self.pixmap)
         
-        '''
-        botón temporal para la transición
-        '''
-        self.button = QPushButton('home',self.centralWidget) 
-        self.button.setGeometry(0,0,100,100)
-        self.button.clicked.connect(self.HomeWindow)
+
+        self.label_img_esquina = QLabel(self)
+        self.label_img_esquina.setGeometry(0,0,0,0)
+        self.pixmap = QPixmap('static/icons/logo_lateral.png')   #Imagen esquina
+        self.label_img_esquina.setPixmap(self.pixmap)
 
         '''
         Botones de home
         '''
-        self.ingresar = QPushButton('INGRESO MANUAL', self.centralWidget)
+        self.ingresar = QToolButton(self.centralWidget)
+        self.ingresar.setText('INGRESO MANUAL')
         self.ingresar.setGeometry(0, 0, 0, 0)
         self.ingresar.setObjectName("button")
-       
+        self.ingresar.setIcon(QIcon('static/icons/icono_entrar'))
+        self.ingresar.setIconSize(QSize(60,60))
+        self.ingresar.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
 
-        self.estadisticas = QPushButton('ESTADISTICAS',self.centralWidget)
+        self.estadisticas = QToolButton(self.centralWidget)
+        self.estadisticas.setText('ESTADISTICAS')
         self.estadisticas.setGeometry(0, 0, 0, 0)
         self.estadisticas.setObjectName("button")
+        self.estadisticas.setIcon(QIcon('static/icons/icono_estadisticas'))
+        self.estadisticas.setIconSize(QSize(60,60))
+        self.estadisticas.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
 
-        self.detener_alarma = QPushButton('DETENER ALARMA',self.centralWidget)
+        self.detener_alarma = QToolButton(self.centralWidget)
+        self.detener_alarma.setText('DETENER ALARMA')
         self.detener_alarma.setGeometry(0, 0, 0, 0)
         self.detener_alarma.setObjectName("button")
+        self.detener_alarma.setIcon(QIcon('static/icons/icono_campana'))
+        self.detener_alarma.setIconSize(QSize(60,60))
+        self.detener_alarma.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
 
-        self.salida_manual = QPushButton('SALIDA MANUAL',self.centralWidget)
+        self.salida_manual = QToolButton(self.centralWidget)
+        self.salida_manual.setText('SALIDA MANUAL')
         self.salida_manual.setGeometry(0, 0, 0, 0)
         self.salida_manual.setObjectName("button")
+        self.salida_manual.setIcon(QIcon('static/icons/icono_salir'))
+        self.salida_manual.setIconSize(QSize(60,60))
+        self.salida_manual.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
 
-        self.configuracion = QPushButton('CONFIGURACIÓN',self.centralWidget)
+        self.configuracion = QToolButton(self.centralWidget)
+        self.configuracion.setText('CONFIGURACIÓN')
         self.configuracion.setGeometry(0, 0, 0, 0)
         self.configuracion.setObjectName("button")
+        self.configuracion.setIcon(QIcon('static/icons/icono_configuraciones'))
+        self.configuracion.setIconSize(QSize(60,60))
+        self.configuracion.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
 
-        self.informacion = QPushButton('INFORMACIÓN',self.centralWidget)
+        self.informacion = QToolButton(self.centralWidget)
+        self.informacion.setText('INFORMACIÓN')
         self.informacion.setGeometry(0, 0, 0, 0)
         self.informacion.setObjectName("button")
+        self.informacion.setIcon(QIcon('static/icons/icono_campana'))
+        self.informacion.setIconSize(QSize(60,60))
+        self.informacion.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
         
-
+        self.timer = QTimer()
+        self.timer.setInterval(3000)
+        self.timer.setSingleShot(True)
+        self.timer.timeout.connect(self.HomeWindow)
+        #self.HomeWindow()
+    
     def HomeWindow(self):
         '''
         Esta función crea la pantalla de home, que contiene 6 botones
         y un logo pequeño
         '''
-        self.label.setGeometry(0,0,0,0) #oculta la imagen
+        
+        self.label_img_central.setGeometry(0,0,0,0) #oculta la imagen
+
+        self.label_img_esquina.setGeometry(15,5,500,100)
+
 
         self.ingresar.setGeometry(44.7, 112.5, 290, 180)
+
         self.estadisticas.setGeometry(367, 112.5, 290, 180)
         self.detener_alarma.setGeometry(689.3, 112.5, 290, 180)
         self.salida_manual.setGeometry(44.7, 348.9, 290, 180)
