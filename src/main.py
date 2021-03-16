fadcbffcd0a2a34485c5618e2c639c2b5c0b2039
Julián Camilo Velandia <jvelandiag@unal.edu.co>
2021-03-16 09:39:06 -0500

    Separación de las vistas y documentación

--- src/main.py
+++ src/main.py
@@ -2,55 +2,61 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
-import sys
 
-
-
-
-class MainApp(QMainWindow):
+class MainWindow(QMainWindow): #Ventana principal
+    '''
+    Esta clase crea la ventana padre, la cual contiene un 
+    logo grande en el centro y cambia pasados n segundos 
+    a la vista HomeWindow
+    '''
     def __init__(self, parent=None, *args):
-        super(MainApp,self).__init__(parent = parent)
+        super(MainWindow,self).__init__(parent = parent)
         
         with open("style.css") as f:
             self.setStyleSheet(f.read())
 
-
         self.setMinimumSize(1024,600)    #tamaño mínimo
         self.setMaximumSize(1024,600)  #tamaño máximo
         self.setWindowTitle('RETEDECON')   #titulo
-
-        self.centralWidget = QWidget()
-        self.setCentralWidget(self.centralWidget)
-        self.centralWidget.setObjectName("window")
+        self.setObjectName("window")    #nombre que enlaza en css
 
 
-        self.ingresar = QPushButton('ingreso',self.centralWidget)
+class HomeWindow(QWidget):
+    '''
+    Esta clase crea la pantalla de home, que contiene 6 botones
+    y un logo pequeño
+    '''
+
+    def __init__(self, parent=None, *args):
+        super(HomeWindow,self).__init__(parent = parent)
+
+        self.ingresar = QPushButton('ingreso')
         self.ingresar.setGeometry(44.7, 112.5, 290, 180)
         self.ingresar.setObjectName("bingresar")
 
-        self.estatus = QPushButton('status',self.centralWidget)
+        self.estatus = QPushButton('status')
         self.estatus.setGeometry(367, 112.5, 290, 180)
         self.estatus.setObjectName("bestatus")
 
 
-        self.estatus = QPushButton('estatus',self.centralWidget)
+        self.estatus = QPushButton('estatus')
         self.estatus.setGeometry(689.3, 112.5, 290, 180)
         self.estatus.setObjectName("bestatus")
 
-        self.estatus = QPushButton('estatus',self.centralWidget)
+        self.estatus = QPushButton('estatus')
         self.estatus.setGeometry(44.7, 348.9, 290, 180)
         self.estatus.setObjectName("bestatus")
 
-        self.estatus = QPushButton('estatus',self.centralWidget)
+        self.estatus = QPushButton('estatus')
         self.estatus.setGeometry(367, 348.9, 290, 180)
         self.estatus.setObjectName("bestatus")
 
-        self.estatus = QPushButton('estatus',self.centralWidget)
+        self.estatus = QPushButton('estatus')
         self.estatus.setGeometry(689.3, 348.9, 290, 180)
         self.estatus.setObjectName("bestatus")
 
 if __name__=='__main__':
     app = QApplication([])
-    window = MainApp()
+    window = MainWindow()
     window.show()
     app.exec_()
