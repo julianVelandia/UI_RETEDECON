56ae49d8cfcc1d03310ee4d3c7b6c779aa80fca4
Julián Velandia <jvelandiag@unal.edu.co>
2021-04-08 13:08:30 -0500

    Bug 'funciones da error' solucionado

--- src/views/botones/boton.py
+++ src/views/botones/boton.py
@@ -2,21 +2,23 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
-from .botones.funciones import Funciones
+from .funciones import Funciones
 
 class Boton(Funciones):
-    def boton_ingresar(self, widget, funcion):
+    def boton_ingresar(self, widget):
         self.ingresar = QToolButton(widget)
         self.ingresar.setText('INGRESO MANUAL')
         self.ingresar.setObjectName("button") #nombre de enlace a css
         self.ingresar.setIcon(QIcon('../../static/icons/icono_entrar')) #icono
         self.ingresar.setIconSize(QSize(60,60))
         self.ingresar.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
-        self.ingresar.clicked.connect(funcion)
+        self.ingresar.clicked.connect(self.Ingresar)
+        self.ingresar.setVisible(False)
 
 
-    def boton_home(self, widget, funcion):
+
+    def boton_home(self, widget):
         self.label_img_esquina = QToolButton(widget)
         self.label_img_esquina.setGeometry(30,5,250,60)        
         self.label_img_esquina.setObjectName("button_home") #nombre de enlace a css
-        self.label_img_esquina.clicked.connect(funcion)
+        self.label_img_esquina.clicked.connect(self.HomeWindow)
--- src/views/botones/funciones.py
+++ src/views/botones/funciones.py
@@ -1,10 +1,12 @@
+
+
+
+
 class Funciones:
     def Ingresar(self):
         self.label_img_central.setVisible(False)  
         self.label_img_esquina.setVisible(True)  
         self.ingresar.setVisible(False)
-        
-        
         self.estadisticas.setVisible(False)
         self.detener_alarma.setVisible(False)
         self.salida_manual.setVisible(False)
@@ -21,3 +23,47 @@ class Funciones:
         self.ingresar_cedula_out.setVisible(False)
         self.retirar.setVisible(False)
         self.Ingresar_guardar_teclado()
+
+    def HomeWindow(self):
+        self.label_img_central.setVisible(False)
+        self.label_img_esquina.setVisible(True)
+        self.ingresar.setVisible(True)
+        self.estadisticas.setVisible(True)
+        self.detener_alarma.setVisible(True)
+        self.salida_manual.setVisible(True)
+        self.configuracion.setVisible(True)
+        self.informacion.setVisible(True)
+        self.ingresar_nombre.setVisible(False)
+        self.ingresar_cedula.setVisible(False)
+        self.ingresar_temp.setVisible(False)
+        self.ingresar_nombre_out.setVisible(False)
+        self.ingresar_cedula_out.setVisible(False)
+        self.ingresar_ingresar.setVisible(False)
+        self.retirar.setVisible(False)
+        self.configuracion_apagar.setVisible(False)
+        self.configuracion_pantalla.setVisible(False)
+        self.configuracion_datos.setVisible(False)
+        self.configuracion_capacidad_text.setVisible(False)
+        self.configuracion_avanzada_user.setVisible(False)
+        self.configuracion_avanzada_pass.setVisible(False)
+        self.configuracion_avanzada.setVisible(False)
+        self.ingresar_Ad_Conf.setVisible(False)
+        self.configuracion_capacidad.setVisible(False)
+        self.new_admin_username.setVisible(False)
+        self.new_admin_password.setVisible(False)
+        self.new_admin.setVisible(False)
+        self.quitar_admin.setVisible(False)
+        self.manual_de_usuario.setVisible(False)
+        self.informacion_fabricante.setVisible(False)
+        self.label_texto_info_fab.setVisible(False)
+        self.img_esquina_2.setVisible(False)
+        self.ingresar_capacidad.setVisible(False)
+        self.agregar_eliminar_admin_boton.setVisible(False)
+        self.cambiar_password_admin_boton.setVisible(False)
+        self.enviar_datos_servidor.setVisible(False)
+        self.info_ocupacion_actual.setVisible(False)
+        self.qr_manual.setVisible(False)
+        self.bar_chart.setVisible(False)
+        self.pie_chart.setVisible(False)
+        self.NotTeclado()
+        self.NotTecladoNumerico()
