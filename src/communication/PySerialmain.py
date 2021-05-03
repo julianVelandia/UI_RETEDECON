dd4792b84bc7d410bd758b527594f2a9d538e85d
SebastianCubides <scubidest@unal.edu.co>
2021-05-03 02:11:19 -0500

    RFIDandIRSensor

--- src/communication/PySerialmain.py
+++ src/communication/PySerialmain.py
@@ -2,11 +2,21 @@ import serial
 arduino = serial.Serial('COM5', 9600)
 
 while True:
-      line = arduino.readline()
-      for lines in line:
-            linea = str(line)
-            line_find=linea.find("Card UID: ")
-            if not line_find == -1:
-                  print(line)
-                  break
+    line = arduino.readline()
+    for lines in line:
+        linea = str(line)
+        uid_find=linea.find("Card UID: ")
+        IRstatus = linea.find("IR ")
+        status = linea[IRstatus+3]
+        if status == '1':
+            isIn = True
+            print(isIn)
+            break
+        if not uid_find == -1 and isIn:
+            #print(line)
+            IDstr = ""
+            for i in range(11):
+                IDstr += linea[uid_find+(i+10)]
+            print(IDstr)
+            break
 arduino.close() #Finalizamos la comunicacion
--- src/communication/ino/serialmain/serialmain.ino
+++ src/communication/ino/serialmain/serialmain.ino
@@ -1,16 +1,50 @@
-int led = 13;
-void setup () {
-   pinMode(led, OUTPUT); //LED 13 como salida
-   Serial.begin(9600); //Inicializo el puerto serial a 9600 baudios
+////////////////////////////////////////////////////////////////////////////////
+#include <SPI.h>
+#include <MFRC522.h>
+#define RST_PIN         9          // Configurable, see typical pin layout above
+#define SS_PIN          10         // Configurable, see typical pin layout above
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+int IRSensor = 2;                  //Define the IRsensor connection
+
+void setup() {
+
+  /////////////////READ RFID///////////////////
+  Serial.begin(9600);                 // Initialize serial communications with the PC
+  while (!Serial);                    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  SPI.begin();                        // Init SPI bus
+  mfrc522.PCD_Init();                 // Init MFRC522
+  delay(5);                           // Optional delay. Some board do need more time after init to be ready, see Readme
+  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
+
+  /////////////////IRSENSOR///////////////////
+  pinMode (IRSensor, INPUT);          //Set IRSensor digital pin 2 as INPUT
 }
 
-void loop () {
-   if (Serial.available()) { //Si está disponible
-      char c = Serial.read(); //Guardamos la lectura en una variable char
-      if (c == 'H') { //Si es una 'H', enciendo el LED
-         digitalWrite(led, HIGH);
-      } else if (c == 'L') { //Si es una 'L', apago el LED
-         digitalWrite(led, LOW);
-      }
-   }
+void loop() {
+
+  ///////////////IRSENSOR////////////////////////
+  int IRstatus = digitalRead(IRSensor);
+  if (IRstatus == 0){
+    String IRID = "IR"; 
+    String IRBus = IRID +' '+ String(IRstatus+1);
+    Serial.println(IRBus);
+    delay(1000);
+  }else{
+    String IRID = "IR"; 
+    String IRBus = IRID +' '+ String(IRstatus-1);
+    Serial.println(IRBus);
+    delay(1000);
+  }
+  
+  /////////////////READ RFID///////////////////
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+  // Dump debug info about the card; PICC_HaltA() is automatically called
+  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
 }
