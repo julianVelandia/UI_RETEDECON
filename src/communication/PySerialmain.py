9f39fd7fff2ec79b18d680f642d4d0a4ad9329bb
SebastianCubides <scubidest@unal.edu.co>
2021-05-06 18:57:01 -0500

    twoINOconnection 1

--- src/communication/PySerialmain.py
+++ src/communication/PySerialmain.py
@@ -1,22 +1,50 @@
 import serial
-arduino = serial.Serial('COM5', 9600)
 
-while True:
-    line = arduino.readline()
-    for lines in line:
-        linea = str(line)
-        uid_find=linea.find("Card UID: ")
-        IRstatus = linea.find("IR ")
-        status = linea[IRstatus+3]
-        if status == '1':
-            isIn = True
-            print(isIn)
-            break
-        if not uid_find == -1 and isIn:
-            #print(line)
-            IDstr = ""
-            for i in range(11):
-                IDstr += linea[uid_find+(i+10)]
-            print(IDstr)
-            break
-arduino.close() #Finalizamos la comunicacion
+class UNO:
+    def __init__(self):
+        self.arduinoUNO = serial.Serial('COM5', 9600)
+
+    def datain(self):
+        while True:
+            line = self.arduinoUNO.readline()
+            linea = str(line)
+            uid_find = linea.find("Card UID: ")
+            IRstatus = linea.find("IR ")
+            status = linea[IRstatus + 3]
+            for lines in line:
+                if status == '1':
+                    isIn = True
+                    print(isIn)
+                    break
+                if not uid_find == -1 :
+                    #print(line)
+                    IDstr = ""
+                    for i in range(11):
+                        IDstr += linea[uid_find+(i+10)]
+                    print(IDstr)
+                    break
+        self.arduinoUNO.close()  # Finalizamos la comunicacion con UNO
+
+class NANO:
+    def __init__(self):
+        self.arduinoNANO = serial.Serial('COM4', 9600)
+
+    def dataout(self):
+        while True:
+            line = self.arduinoNANO.readline()
+            for lines in line:
+                linea = str(line)
+                uid_find = linea.find("Card UID: ")
+                if not uid_find == -1:
+                    # print(line)
+                    IDstr = ""
+                    for i in range(11):
+                        IDstr += linea[uid_find + (i + 10)]
+                    print(IDstr)
+                    break
+        self.arduinoNANO.close() #Finalizamos la comunicacion con NANO
+
+uno = UNO()
+uno.datain()
+nano = NANO()
+nano.dataout()
---
+++ src/communication/ino/serialmainOut/serialmainOut.ino
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////////////////////
+#include <SPI.h>
+#include <MFRC522.h>
+#define RST_PIN         9          // Configurable, see typical pin layout above
+#define SS_PIN          10         // Configurable, see typical pin layout above
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+
+void setup() {
+
+  /////////////////READ RFID///////////////////
+  Serial.begin(9600);                 // Initialize serial communications with the PC
+  while (!Serial);                    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  SPI.begin();                        // Init SPI bus
+  mfrc522.PCD_Init();                 // Init MFRC522
+  delay(5);                           // Optional delay. Some board do need more time after init to be ready, see Readme
+  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
+}
+
+void loop() {
+  /////////////////READ RFID///////////////////
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+  // Dump debug info about the card; PICC_HaltA() is automatically called
+  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+}
