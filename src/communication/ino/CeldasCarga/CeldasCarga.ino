01bc9b3935d9f80271aeacb85dfa26c2ff061c7b
SebastianCubides <scubidest@unal.edu.co>
2021-05-16 21:18:49 -0500

    CeldasCargaUPDT

--- src/communication/ino/CeldasCarga/CeldasCarga.ino
+++ src/communication/ino/CeldasCarga/CeldasCarga.ino
@@ -7,38 +7,32 @@
  Arduino pin 5V -> HX711 VCC
  Arduino pin GND -> HX711 GND 
 */
-
 #include "HX711.h"
-
 HX711 scale(5, 6);
-
-float calibration_factor = 45100; // this calibration factor is adjusted according to my load cell
+float calibration_factor = 48100; // this calibration factor is adjusted according to my load cell
 float units;
-
-
 
 void setup() {
   Serial.begin(9600);
-   scale.set_scale();
+  scale.set_scale();
   scale.tare();  //Reset the scale to 0
- pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(2, OUTPUT);
   long zero_factor = scale.read_average(); //Get a baseline reading
- }
+  Serial.println(zero_factor);
 
+}
 void loop() {
 
   scale.set_scale(calibration_factor); //Adjust to this calibration factor
-
-
   units = scale.get_units(), 10;
-  if (units < 0)
-  {
+    Serial.println(units);
+    delay(500);
+    
+  if (units < 0){
     units = 0.00;
   }
-
-  if (units > 1)
-    {
-      digitalWrite(LED_BUILTIN, HIGH);
-    }
   
+  if (units > 1){
+    digitalWrite(2, HIGH);
+  }
 }
